4c5e8ec7ac4e7e3733ddbd20d4d35c13d3a058f6
Kshitij Bajracharya <bkshitij.dev@gmail.com>
2024-11-21 21:35:15 +0545

    Update service for top movers table

--- app/services/stockMarketHistoryService.ts
+++ app/services/stockMarketHistoryService.ts
@@ -18,8 +18,8 @@ const stockMarketHistoryService = {
       return response.json();
     },
 
-    getTopMoversData: async (slug: string) => {
-      const response = await fetch(`${API_ENDPOINTS.STOCK_MARKET_HISTORY}/top-${slug}`);
+    getTopMoversData: async (slug: string, days: number) => {
+      const response = await fetch(`${API_ENDPOINTS.STOCK_MARKET_HISTORY}/top-${slug}?days=${days}`);
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
